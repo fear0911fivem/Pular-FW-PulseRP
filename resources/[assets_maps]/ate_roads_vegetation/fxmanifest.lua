79ec5ad8bb75cd02e2622b38c9acd2a36eed9cbe
RedrumDOJ <redrumdoj@gmail.com>
2026-05-10 17:55:05 -0400

    Road Vegetation
    - In the name road vegetations to the city

---
+++ resources/[assets_maps]/ate_roads_vegetation/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+this_is_a_map 'yes'
+author 'Atenea'
+description 'Map Road Vegetation'
+version '1.0.2'
+
+---------------------------------------------------------------------------
+-- INCLUDED FILES
+---------------------------------------------------------------------------
+--files {
+    --'stream/**/*.ytyp',
+--}
+
+--data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
+dependency '/assetpacks'
+
+
